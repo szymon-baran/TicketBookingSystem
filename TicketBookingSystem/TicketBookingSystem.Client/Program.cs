9e61d5f6314322612ea53dff5d0d871689adc463
Szymon Baran <szymon.w.baran@gmail.com>
2022-10-17 16:33:11 +0200

    Add project files.

---
+++ TicketBookingSystem/TicketBookingSystem.Client/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TicketBookingSystem.Client;
+
+namespace TicketBookingSystem.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddHttpClient("TicketBookingSystem.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+            // Supply HttpClient instances that include access tokens when making requests to the server project
+            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("TicketBookingSystem.ServerAPI"));
+
+            builder.Services.AddApiAuthorization();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ TicketBookingSystem/TicketBookingSystem.Server.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TicketBookingSystem.Server.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
