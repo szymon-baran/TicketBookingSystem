c2d33cd02bece882c613998a88a1795221b0fe22
Daniel Bednarz <daniosdb@gmail.com>
2022-10-29 15:18:41 +0200

    organised login and register page; removed duplicated files

--- TicketBookingSystem/TicketBookingSystem.Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TicketBookingSystem/TicketBookingSystem.Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -87,7 +87,7 @@ namespace TicketBookingSystem.Server.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Hasło")]
             public string Password { get; set; }
 
             /// <summary>
@@ -95,8 +95,8 @@ namespace TicketBookingSystem.Server.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Potwórz hasło")]
+            [Compare("Password", ErrorMessage = "Hasła nie są identyczne.")]
             public string ConfirmPassword { get; set; }
         }
 
